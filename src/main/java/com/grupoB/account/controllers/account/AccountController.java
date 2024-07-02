62176154e94d25506bc8c61432799a02dbb567ad
matheus-battiston <matheus_battiston@hotmail.com>
2024-07-02 20:46:47 -0300

    correcao yml deploy e alteracao para teste do deploy na aplicacao

--- src/main/java/com/grupoB/account/controllers/account/AccountController.java
+++ src/main/java/com/grupoB/account/controllers/account/AccountController.java
@@ -21,7 +21,7 @@ public class AccountController {
 
     @GetMapping("/testdeploy")
     public String testDeploy() {
-        return "Deploy funcionou";
+        return "Deploy funcionou com o terrafor";
     }
     @GetMapping("/preferences")
     public ResponseEntity<?> getAccount(@RequestParam("id") Integer id){
