0c77e4aa9e694feb9b99cd0145520f68ae4ec143
matheus-battiston <matheus_battiston@hotmail.com>
2024-07-09 12:38:15 -0300

    Terraform no github actions / alteracao no endpoint testdeploy

--- src/main/java/com/grupoB/account/controllers/account/AccountController.java
+++ src/main/java/com/grupoB/account/controllers/account/AccountController.java
@@ -21,7 +21,7 @@ public class AccountController {
 
     @GetMapping("/testdeploy")
     public String testDeploy() {
-        return "Deploy";
+        return "Deploy com terraform";
     }
     @GetMapping("/preferences")
     public ResponseEntity<?> getAccount(@RequestParam("id") Integer id){
